c56941857dfd7590f5d4733264c8ec0059a6efbd
Lexx <joncanales2@gmail.com>
2020-11-05 15:32:54 -0500

    creating player character base form.

--- client/src/containers/DmForm/DmForm.jsx
+++ client/src/containers/DmForm/DmForm.jsx
@@ -11,21 +11,35 @@ function DmForm() {
           <div className="row vertical-spacer-md">
             <div className="col s6">
               <label for="username">
-                <p className="form-text">Username</p>
+                <p className="form-text">roomName</p>
               </label>
               <input id="username" type="text" className="validate" />
               <label for="email">
-                <p className="form-text">Email</p>
+                <p className="form-text">tagLine</p>
               </label>
               <input id="email" type="email" className="validate" />
             </div>
 
             <div className="col s6">
-              <div>
+              <div className="col s12">
                 <p>Avatar:</p>
-                <img className="content-border col s12" src={placeholderImg} />
-                <div className="content-border">DROP DOWN</div>
+                <img
+                  className="content-border col s12 image-true"
+                  src={placeholderImg}
+                />
               </div>
+
+              <select className="browser-default">
+                <option value="" disabled selected>
+                  Select Your Avatar
+                </option>
+                <option value="1">Bard</option>
+                <option value="2">Cleric</option>
+                <option value="3">Fighter</option>
+                <option value="4">Paladin</option>
+                <option value="5">Thief</option>
+                <option value="6">Wizard</option>
+              </select>
             </div>
           </div>
 
@@ -40,8 +54,7 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>campaigns</p>
                     </span>
                   </label>
                 </p>
@@ -51,8 +64,7 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>oneshots</p>
                     </span>
                   </label>
                 </p>
@@ -62,44 +74,7 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
-                    </span>
-                  </label>
-                </p>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col s4">
-                <p>
-                  <label>
-                    <input type="checkbox" />
-                    <span>
-                      {" "}
-                      <p>Red</p>{" "}
-                    </span>
-                  </label>
-                </p>
-              </div>
-              <div className="col s4">
-                <p>
-                  <label>
-                    <input type="checkbox" />
-                    <span>
-                      {" "}
-                      <p>Red</p>{" "}
-                    </span>
-                  </label>
-                </p>
-              </div>
-              <div className="col s4">
-                <p>
-                  <label>
-                    <input type="checkbox" />
-                    <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>homebrew</p>
                     </span>
                   </label>
                 </p>
@@ -112,8 +87,7 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>byTheBook</p>
                     </span>
                   </label>
                 </p>
@@ -123,8 +97,7 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>rpersonly</p>
                     </span>
                   </label>
                 </p>
@@ -134,8 +107,40 @@ function DmForm() {
                   <label>
                     <input type="checkbox" />
                     <span>
-                      {" "}
-                      <p>Red</p>{" "}
+                      <p>norestriction</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col s4">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>displaydice</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s4">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>lvl1only</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s4">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>voyuerallowed</p>
                     </span>
                   </label>
                 </p>
@@ -153,8 +158,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Monday</p>{" "}
+                        <p>Monday</p>
                       </span>
                     </label>
                   </p>
@@ -164,8 +168,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Tuesday</p>{" "}
+                        <p>Tuesday</p>
                       </span>
                     </label>
                   </p>
@@ -175,8 +178,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Wednesday</p>{" "}
+                        <p>Wednesday</p>
                       </span>
                     </label>
                   </p>
@@ -186,8 +188,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Thursday</p>{" "}
+                        <p>Thursday</p>
                       </span>
                     </label>
                   </p>
@@ -200,8 +201,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Friday</p>{" "}
+                        <p>Friday</p>
                       </span>
                     </label>
                   </p>
@@ -211,8 +211,7 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Saturday</p>{" "}
+                        <p>Saturday</p>
                       </span>
                     </label>
                   </p>
@@ -222,12 +221,22 @@ function DmForm() {
                     <label>
                       <input type="checkbox" />
                       <span>
-                        {" "}
-                        <p>Sunday</p>{" "}
+                        <p>Sunday</p>
                       </span>
                     </label>
                   </p>
                 </div>
+              </div>
+              <div className="row vertical-spacer-sm">
+                <div className="col s4 "></div>
+                <button className="vertical-spacer-sm waves-effect waves-light btn col s3">
+                  Cancel
+                </button>
+                <div className="col s1 "></div>
+                <button className="vertical-spacer-sm waves-effect waves-light btn col s3">
+                  Create Account
+                </button>
+                <div className="col s1 "></div>
               </div>
             </div>
           </div>
