2c3bb810356c3e282d83e2f27e5be62e739d5642
Lexx <joncanales2@gmail.com>
2020-11-05 11:58:42 -0500

    putting together basic react router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { Router } from "express";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar"
 import SigninPage from "./containers/SigninPage/SigninPage"
+import {BrowserRouter as Nav, Router, Router, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+        <Router>
+        <Switch>
+        <Route exact path ="/" component={SigninPage} />
+
+
+        </Switch>
+        </Router>
         <Navbar />
-        <SigninPage />
+        
     </div>
   );
 }
