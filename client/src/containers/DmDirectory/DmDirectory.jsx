6f84ec66bb59622a0aca78f40d03ea48ede979d0
Lexx <joncanales2@gmail.com>
2020-11-05 21:04:30 -0500

    base display for the dmdirectory is finished.

--- client/src/containers/DmDirectory/DmDirectory.jsx
+++ client/src/containers/DmDirectory/DmDirectory.jsx
@@ -1,35 +1,211 @@
-import React from 'react';
+import { dirname } from "path";
+import React from "react";
+import Dmaster from "../../components/Dmaster/Dmaster"
 
 function DmDirectory() {
-    return (
-        <>
-        <div className="container">
-            <form className="content-border">
-                <input type="text"/>
-                <input className="vertical-spacer-sm waves-effect waves-light btn" type="submit"/>
-            </form>
-        </div>
-        
-        <div className="row">
-            <div className="col s4">
-                <h4>Category</h4>
-                                  <p>
-                    <label>
-                      <input type="checkbox" />
-                      <span>
-                        <p>Friday</p>
-                      </span>
-                    </label>
-                  </p>
-                
-                left side hello</div>
-            
-            <div className="col s8">right side hello</div>
+  return (
+    <>
+      <div className="container">
+        <form className="content-border">
+          <input type="text" />
+          <input
+            className="vertical-spacer-sm waves-effect waves-light btn"
+            type="submit"
+          />
+        </form>
+      </div>
 
+      <div className="row">
+        <div className="col s3 content-border">
+          <div className="row">
+            <div className="col s12">
+              <h5>Category:</h5>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>campaigns</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>oneshots</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>homebrew</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>byTheBook</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>rpersonly</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>norestriction</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>displaydice</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>lvl1only</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>voyuerallowed</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+              <h5>Availability:</h5>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Monday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Tuesday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Wednesday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Thursday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Friday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Saturday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
+              <div className="col s12">
+                <p>
+                  <label>
+                    <input type="checkbox" />
+                    <span>
+                      <p>Sunday</p>
+                    </span>
+                  </label>
+                </p>
+              </div>
 
+            </div>
+          </div>
         </div>
-        </>
-    );
-};
+
+        <div className="col s9 content-border">
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+                    <Dmaster />
+    
+    
+            </div>
+      </div>
+    </>
+  );
+}
 
 export default DmDirectory;
