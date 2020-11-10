2c4c3679ffcac1b8d5808d954e105c2e058a5845
Archangel <nathapcal@gmail.com>
2020-11-10 17:42:38 -0500

    working on APP.js with passing token in

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import UserContext from "./utils/UserContext";
 import Navbar from "./components/Navbar/Navbar";
 import SigninPage from "./containers/SigninPage/SigninPage";
 import NewUser1 from "./containers/NewUser1/NewUser1";
-import DmForm from "./containers/DmForm/DmForm";
-import PcForm from "./containers/PcForm/PcForm";
+// import DmForm from "./containers/DmForm/DmForm";
+// import PcForm from "./containers/PcForm/PcForm";
 import DmDirectory from "./containers/DmDirectory/DmDirectory";
 import DmOne from "./containers/DmOne/DmOne";
 import UpdateForm from "./containers/UpdateForm/UpdateForm";
@@ -14,10 +14,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthContext from "./context/AuthContext";
 import "materialize-css";
 import { setAxiosDefaults } from "./utils/axiosDefaults";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 // import { Button, Card, Row, Col } from "react-materialize";
 
 function App() {
-  const [jwt, setJwt] = useState();
+  const [jwt, setJwt] = useState({
+    token: undefined,
+    userName: undefined,
+  });
   const [userId, setUserId] = useState("");
 
   useEffect(() => {
@@ -25,7 +29,7 @@ function App() {
     if (localJwt) {
       setJwt(localJwt);
     }
-  });
+  }, []);
 
   useEffect(() => {
     if (jwt) {
@@ -36,26 +40,30 @@ function App() {
 
   return (
     <div className="App">
-       <UserContext.Provider value={{ userId, setUserId }}>
-      <Router>
-       
-          <AuthContext.Provider value={{ jwt, setJwt }}>
+      <AuthContext.Provider value={{ jwt, setJwt }}>
+        <UserContext.Provider value={{ userId, setUserId }}>
+          <Router>
             <Navbar />
             <Switch>
               <Route exact path="/" component={SigninPage} />
               <Route exact path="/NewUser" component={NewUser1} />
-              <Route exact path="/PcForm/:id" component={PcForm} />
-              <Route exact path="/DmForm/:id" component={DmForm} />
+              {/* <ProtectedRoute exact path="/NewUser" component={NewUser1} /> */}
+              {/* <Route exact path="/PcForm/:id" component={PcForm} /> */}
+              {/* <Route exact path="/DmForm/:id" component={DmForm} /> */}
               <Route exact path="/DmDirectory" component={DmDirectory} />
               <Route exact path="/DmOne/:id" component={DmOne} />
-              <Route exact path="/UpdateForm/:id" component={UpdateForm} />
+              {/* <Route exact path="/UpdateForm/:id" component={UpdateForm} /> */}
+              <ProtectedRoute
+                exact
+                path="/UpdateForm/:id"
+                component={UpdateForm}
+              />
               <Route exact path="/ThreeD/:id" component={ThreeD} />
               <Route path="/" component={SigninPage} />
             </Switch>
-          </AuthContext.Provider>
-
-      </Router>
-      </UserContext.Provider>
+          </Router>
+        </UserContext.Provider>
+      </AuthContext.Provider>
     </div>
   );
 }
