045c178a7f7f2f6f3ee994cc29802077bfee2d38
ad-fleming <adfleming@gmail.com>
2020-11-11 14:58:10 -0500

    took function out of logout button

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -1,23 +1,8 @@
 import React from "react"
-import { useHistory } from "react-router-dom"
 import "./logoutbutton.css"
 import { Link } from "react-router-dom"
 
 export default function HomeButton(){
-
-    const history = useHistory()
-
-    function handleLogout(){
-
-        const logOut = async function(){
-            localStorage.removeItem("jwt")
-            await history.push("/")
-
-        }
-
-        logOut();
-
-    }
 
     return(
         <li className="tab"><Link to="/">Logout</Link></li>
