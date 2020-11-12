52ad2911da2dea8d6bf2ce44d641bdccaa4bf2b4
ad-fleming <adfleming@gmail.com>
2020-11-12 13:53:27 -0500

    saving changes for incomring submit

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./logoutbutton.css";
+import AuthContext from "../../context"
 
 export default function HomeButton() {
+
+  
+
   return (
     <li className="tab">
       <a href="/"> Logout </a>
