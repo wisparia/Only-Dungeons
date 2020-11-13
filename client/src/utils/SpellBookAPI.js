cb7f6cb2b06b0cf91507ce75d4f485425c734418
Archangel <nathapcal@gmail.com>
2020-11-12 20:08:19 -0500

    able to populate and dynamically render classes/proficncies

--- client/src/utils/SpellBookAPI.js
+++ client/src/utils/SpellBookAPI.js
@@ -3,6 +3,10 @@ import axios from "axios";
 export default {
       //Gets the class information from the DnD API
   getDndInfo: function (query){
-    return axios.get(`https://www.dnd5eapi.co/api/classes/rogue`)
-  }
+    return axios.get(`https://www.dnd5eapi.co/api/classes/${query}`)
+  },
+
+  // getDndLanguages: function (query1, query2) {
+  //   return axios.get(`https://www.dnd5eapi.co/api/${query1}/${query2}`)
+  // }
 }
