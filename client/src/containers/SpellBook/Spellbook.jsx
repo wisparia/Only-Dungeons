67b13c1bc0c0520fb3a5f5f7acef5929c10b7a2c
Archangel <nathapcal@gmail.com>
2020-11-12 21:36:51 -0500

    after useEffect loads, each seperate states are filled

--- client/src/containers/SpellBook/Spellbook.jsx
+++ client/src/containers/SpellBook/Spellbook.jsx
@@ -3,10 +3,15 @@ import API from "../../utils/SpellBookAPI";
 import "./Spellbook.css";
 
 function Spellbook() {
-  const [classInfo, setClassInfo] = useState([]);
-  const [proficiencies, setProficiencies] = useState([]);
+  const [characterClassState, setCharacterClassState] = useState([]);
+  const [monsterState, setMonsterState] = useState([]);
+  const [raceState, setraceState] = useState([]);
+  const [equipmentState, setequipmentState] = useState([]);
+  const [spellState, setspellState] = useState([]);
+  const [armorState, setarmorState] = useState([]);
+  const [weaponState, setweaponState] = useState([]);
+  const [rulesState, setrulesState] = useState([]);
 
-  
   useEffect(() => {
     getClassesArray();
     getMonstersArray();
@@ -31,63 +36,109 @@ function Spellbook() {
   //     .catch((err) => console.log(err));
   // }
 
+    // console.log("++++++++++++++++++++++++++++++++++++");
+    // console.log(weaponState);
+    // console.log("++++++++++++++++++++++++++++++++++++");
+
   function getClassesArray() {
-    API.getDndClasses().then((res) => {
-      console.log(res.data.results)
-    }).catch((err) => {console.log(err)})
+    API.getDndClasses()
+      .then((res) => {
+        // console.log(res.data.results);
+        const classInformation = res.data.results;
+        setCharacterClassState(classInformation);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   function getMonstersArray() {
-    API.getDndMonsters().then((res) => {
-      console.log(res.data.results)
-    }).catch((err) => {console.log(err)})
+    API.getDndMonsters()
+      .then((res) => {
+        // console.log(res.data.results);
+        const monsters = res.data.results;
+        setMonsterState(monsters);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getRacesArray(){
-    API.getDndRaces().then((res) => {
-      console.log(res.data.results)
-      // target name for mapping
-      // url for click API call query
-    }).catch((err) => {console.log(err)})
+  function getRacesArray() {
+    API.getDndRaces()
+      .then((res) => {
+        // console.log(res.data.results);
+        // target name for mapping
+        // url for click API call query
+        const race = res.data.results;
+        setraceState(race);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getDndEqupArray(){
-    API.getDndEquipment().then((res) => {
-      console.log(res.data.results)
-    }).catch((err) => {console.log(err)})
+  function getDndEqupArray() {
+    API.getDndEquipment()
+      .then((res) => {
+        // console.log(res.data.results);
+        const equipment = res.data.results;
+        setequipmentState(equipment);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getDndSpellsArray(){
-    API.getDndSpells().then((res) => {
-      console.log(res.data.results)
-    }).catch((err) => {console.log(err)})
+  function getDndSpellsArray() {
+    API.getDndSpells()
+      .then((res) => {
+        // console.log(res.data.results);
+        const spells = res.data.results;
+        setspellState(spells);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getDndArmorArray(){
-    API.getDndArmor().then((res) => {
-      console.log(res.data.equipment)
-    }).catch((err) => {console.log(err)})
+  function getDndArmorArray() {
+    API.getDndArmor()
+      .then((res) => {
+        // console.log(res.data.equipment);
+        const armor = res.data.equipment;
+        setarmorState(armor);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getDndWeaponArray(){
-    API.getDndWeapon().then((res) => {
-      console.log(res.data.equipment)
-    }).catch((err) => {console.log(err)})
+  function getDndWeaponArray() {
+    API.getDndWeapon()
+      .then((res) => {
+        // console.log(res.data.equipment);
+        const weapon = res.data.equipment;
+        setweaponState(weapon);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  function getDndRules(){
-    API.getDndRules().then((res) => {
-      console.log(res.data.results)
-    }).catch((err) => {console.log(err)})
+  function getDndRules() {
+    API.getDndRules()
+      .then((res) => {
+        // console.log(res.data.results);
+        const rules = res.data.results;
+        setrulesState(rules);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-
-
-
-
-
-
-// need to add more information 
+  // need to add more information
 
   return (
     <>
@@ -112,7 +163,7 @@ function Spellbook() {
             </select>
 
             <div className="">
-              <h3 className="col s12 m6 l4">{classInfo.name}</h3>
+              {/* <h3 className="col s12 m6 l4">{classInfo.name}</h3> */}
 
               <ul>
                 {/* {proficiencies.map((proficiencyType, i) => (
