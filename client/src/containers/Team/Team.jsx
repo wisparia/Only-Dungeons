0f986cfc4af0c435c3d79dbcb31db9688a7fbb64
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-11-13 10:29:22 -0500

    added test pictures in teams

--- client/src/containers/Team/Team.jsx
+++ client/src/containers/Team/Team.jsx
@@ -7,7 +7,7 @@ function Team() {
     <div className="container">
     <div className="row content-border center mainbox">
         <div className="col s12 m12 l4">
-          <h4>image here</h4>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Mt4fb-h78sl556xbhcsk3QHaGz%26pid%3DApi&f=1" height ="150px" width ="150px"  alt ="gangster grandma"></img>
           <h4>Jonathan Canales</h4>
           </div>
           <div className="col s12 l8">
@@ -16,8 +16,8 @@ function Team() {
 
         <div className="row content-border center mainbox">
         <div className="col s12 m12 l4">
-          <h4>image here</h4>
-          <h4>Alex Flemming</h4>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.explicit.bing.net%2Fth%3Fid%3DOIP.tCGcY_S1jJ9cur7ENFYVEQHaIM%26pid%3DApi&f=1" height ="150px" width ="150px"  alt ="gangster grandma"></img>
+          <h4>Alex Fleming</h4>
           </div>
           <div className="col s12 l8">
           <p>Yay quick description</p>
@@ -25,8 +25,8 @@ function Team() {
 
         <div className="row content-border center mainbox">
         <div className="col s12 m12 l4">
-          <h4>image here</h4>
-          <h4>Nathan</h4>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fst3.depositphotos.com%2F3320701%2F12682%2Fi%2F950%2Fdepositphotos_126825068-stock-photo-male-sorcerer-casting-a-spell.jpg&f=1&nofb=1" height ="150px" width ="150px"  alt ="gangster grandma"></img>
+          <h4>Nathan Calle</h4>
           </div>
           <div className="col s12 l8">
           <p>Yay quick description</p>
@@ -34,7 +34,7 @@ function Team() {
 
         <div className="row content-border center mainbox">
         <div className="col s12 m12 l4">
-          <h4>image here</h4>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F37%2Ffe%2F11%2F37fe11a4862feed3990a8c6dee1dab51.jpg&f=1&nofb=1" height ="150px" width ="150px"  alt ="gangster grandma"></img>
           <h4>Sami Sully</h4>
           </div>
           <div className="col s12 l8">
@@ -43,7 +43,7 @@ function Team() {
 
         <div className="row content-border center mainbox">
         <div className="col s12 m12 l4">
-          <h4>image here</h4>
+          <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-336XSJI49e0%2FUjQRy7nEdhI%2FAAAAAAAABBs%2FCojTRfmJnuw%2Fs200%2FGangster%2BGranny.png&f=1&nofb=1" height ="150px" width ="150px"  alt ="gangster grandma"></img>
           <h4>Brandon Walker</h4>
           </div>
           <div className="col s12 l8">
