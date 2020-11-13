60d390fbe43f121dc5b12775db5eccb01f3bf770
Lexx <joncanales2@gmail.com>
2020-11-12 21:38:50 -0500

    merging update for spellbook API.

--- client/src/utils/SpellBookAPI.js
+++ client/src/utils/SpellBookAPI.js
@@ -26,22 +26,37 @@ export default {
     return axios.get(`https://www.dnd5eapi.co/api/spells`)
   },
 
+  getDndSpecificSpells: function(query)  {
+    return axios.get(`https://www.dnd5eapi.co/api/spells/${query}`)
+    // .desc[0] // .higher_level[0] // .range // .material // .ritual // .duration // .concentration //  
+  },
+
   getDndArmor: function() {
     return axios.get(`https://www.dnd5eapi.co/api/equipment-categories/armor`)
+  },
+ 
+  getSpecificArmor: function(query) {
+    return axios.get(`https://www.dnd5eapi.co/api/equipment/${query}`)
+// .armor_category // armor_class.base // armor_class.dex_bonus // armor_class.max_bonus
   },
 
   getDndWeapon: function() {
     return axios.get(`https://www.dnd5eapi.co/api/equipment-categories/weapon`)
   },
 
+  getSpecificWeapon: function(query) {
+    return axios.get(`https://www.dnd5eapi.co/api/equipment/${query}`)
+// .weapon_category // .weapon_range // .damage.damage_dice // .damage.damage_type.name  
+  },
+
   getDndRules: function() {
     return axios.get(`https://www.dnd5eapi.co/api/rules`)
+  },
+
+  getSpecificRules: function(query) {
+    return axios.get(`https://www.dnd5eapi.co/api/rules${query}`)
+// subsection targetting, GOOD LUCK
   }
-
-
-  // weapons
-  // armor
-  // rules
 }
 
 
