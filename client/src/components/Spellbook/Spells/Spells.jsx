8c1322cee5b28eaf5b125f619271303f4c95ce04
Archangel <nathapcal@gmail.com>
2020-11-14 14:54:08 -0500

    moved from containers into components for spellbook

--- client/src/components/Spellbook/Spells/Spells.jsx
+++ client/src/components/Spellbook/Spells/Spells.jsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../../utils/API";
 
-function Spells() {
+const BookOfSpells = () => {
+  const [spells, setSpells] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const spellBook = function () {
+    API.getSpells().then((res) => {
+      console.log(res.data);
+      setSpells(res.data);
+      setLoading(false);
+      // console.log(spellsToAdd);
+    });
+    console.log(spells);
+  };
+
+  useEffect(() => {
+    spellBook();
+  }, []);
+
   return (
     <div>
-      <h1>This is the Spells componenet</h1>
+      {!loading ? (
+        spells.slice(10 * page, 10 * page + 10).map((element) => {
+          return (
+            <div>
+              <h4>{element.name}</h4>
+              <p>
+                Level {element.level} {element.school.name}
+              </p>
+              <p>
+                Available to:
+                <ul>
+                  <>
+                    {element.classes.map((element) => (
+                      <li>{element.name}</li>
+                    ))}
+                  </>
+                </ul>
+              </p>
+
+              <p>Duration: {element.duration}</p>
+              <p>Range: {element.range}</p>
+              <p>Description: {element.desc[0]}</p>
+            </div>
+          );
+        })
+      ) : (
+        <>
+          <div class="spinner-layer spinner-yellow">
+            <div class="circle-clipper left">
+              <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+              <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+              <div class="circle"></div>
+            </div>
+          </div>
+          <h2>Loading Spellbook</h2>
+        </>
+      )}
+      {!loading ? (
+        <button onClick={() => setPage(page + 1)}>Click To Increase</button>
+      ) : null}
     </div>
   );
-}
+};
 
-export default Spells;
+export default BookOfSpells;
