77e2faa13faa6f5dfc6ba544388c8f94db438455
Lexx <joncanales2@gmail.com>
2020-11-14 16:22:59 -0500

    randomized stone materials for crypt scene.

--- client/src/containers/ThreeD/graveScene/graveScene.jsx
+++ client/src/containers/ThreeD/graveScene/graveScene.jsx
@@ -7,46 +7,16 @@ import Stone5 from "./Stone5.png"
 
 const graveScene = (scene, camera, renderer, locX, locY, locZ) => {
 
+    let stoneTypeArray = [Stone1, Stone2, Stone3, Stone4, Stone5 , Stone3, Stone4, Stone5];
+    let stoneType = stoneTypeArray[(Math.floor(Math.random()*8))]
+
     const createGrave1 = (locX, locY, locZ) => {
         const geometry = new THREE.BoxGeometry( 8, 20, 5 );
-        let texture = new THREE.TextureLoader().load(Stone1)
+        let texture = new THREE.TextureLoader().load(stoneType)
         const material = new THREE.MeshBasicMaterial( {map: texture} );
         const graveStone = new THREE.Mesh( geometry, material );
         scene.add(graveStone);
         // graveStone.rotateX(10)
-        graveStone.rotateY(180)
-        graveStone.position.set(locX, (locY+10), locZ)
-    }
-
-    const createGrave2 = (locX, locY, locZ) => {
-        const geometry = new THREE.BoxGeometry( 8, 20, 5 );
-        let texture = new THREE.TextureLoader().load(Stone2)
-        const material = new THREE.MeshBasicMaterial( {map: texture} );
-        const graveStone = new THREE.Mesh( geometry, material );
-        scene.add(graveStone);
-        // graveStone.rotateX(10)
-        graveStone.rotateY(180)
-        graveStone.position.set(locX, (locY+10), locZ)
-    }
-
-    const createGrave3 = (locX, locY, locZ) => {
-        const geometry = new THREE.BoxGeometry( 8, 20, 5 );
-        let texture = new THREE.TextureLoader().load(Stone3)
-        const material = new THREE.MeshBasicMaterial( {map: texture} );
-        const graveStone = new THREE.Mesh( geometry, material );
-        scene.add(graveStone);
-        graveStone.rotateX(.1)
-        graveStone.rotateY(180)
-        graveStone.position.set(locX, (locY+10), locZ)
-    }
-
-    const createGrave4 = (locX, locY, locZ) => {
-        const geometry = new THREE.BoxGeometry( 8, 20, 5 );
-        let texture = new THREE.TextureLoader().load(Stone5)
-        const material = new THREE.MeshBasicMaterial( {map: texture} );
-        const graveStone = new THREE.Mesh( geometry, material );
-        scene.add(graveStone);
-        graveStone.rotateX(.1)
         graveStone.rotateY(180)
         graveStone.position.set(locX, (locY+10), locZ)
     }
@@ -69,86 +39,86 @@ const graveScene = (scene, camera, renderer, locX, locY, locZ) => {
     }
 
     const  createRow1 = (locX, locY, locZ) => { 
-    createGrave4(locX, locY, locZ)
-    createGrave3((locX-20), locY, locZ)
-    // createGrave2((locX+20), locY, locZ)
-    createGrave2((locX-40), locY, locZ)
-    createGrave2((locX+40), locY, locZ)
-    createGrave2((locX-60), locY, locZ)
-    createGrave3((locX+60), locY, (locZ+5))
-    createGrave4((locX-80), locY, locZ)
-    createGrave2((locX+80), locY, locZ)
-    createGrave4((locX-100), locY, (locZ))
-    createGrave3((locX+120), locY, (locZ))
-    createGrave2((locX-120), locY, (locZ))
-    createGrave3((locX-140), locY, (locZ))
+    createGrave1(locX, locY, locZ)
+    createGrave1((locX-20), locY, locZ)
+    // createGrave1((locX+20), locY, locZ)
+    createGrave1((locX-40), locY, locZ)
+    createGrave1((locX+40), locY, locZ)
+    createGrave1((locX-60), locY, locZ)
+    createGrave1((locX+60), locY, (locZ+5))
+    createGrave1((locX-80), locY, locZ)
+    createGrave1((locX+80), locY, locZ)
+    createGrave1((locX-100), locY, (locZ))
+    createGrave1((locX+120), locY, (locZ))
+    createGrave1((locX-120), locY, (locZ))
+    createGrave1((locX-140), locY, (locZ))
     }
     const  createRow2 = (locX, locY, locZ) => { 
-    createGrave3(locX, locY, (locZ+40))
-    createGrave3((locX-20), locY, (locZ+40))
-    createGrave4((locX+20), locY, (locZ+40))
-    createGrave2((locX-40), (locY-4), (locZ+40))
-    createGrave3((locX+40), locY, (locZ+35))
-    createGrave2((locX-60), locY, (locZ+40))
-    createGrave2((locX+60), locY, (locZ+40))
-    createGrave4((locX-80), locY, (locZ+40))
-    createGrave2((locX+80), locY, (locZ+40))
-    createGrave3((locX+100), locY, (locZ+40))
-    // createGrave3((locX-100), locY, (locZ+40))
-    createGrave3((locX+120), locY, (locZ+40))
+    createGrave1(locX, locY, (locZ+40))
+    createGrave1((locX-20), locY, (locZ+40))
+    createGrave1((locX+20), locY, (locZ+40))
+    createGrave1((locX-40), (locY-4), (locZ+40))
+    createGrave1((locX+40), locY, (locZ+35))
+    createGrave1((locX-60), locY, (locZ+40))
+    createGrave1((locX+60), locY, (locZ+40))
+    createGrave1((locX-80), locY, (locZ+40))
+    createGrave1((locX+80), locY, (locZ+40))
+    createGrave1((locX+100), locY, (locZ+40))
+    // createGrave1((locX-100), locY, (locZ+40))
+    createGrave1((locX+120), locY, (locZ+40))
     createGrave1((locX-120), locY, (locZ+40))
-    createGrave2((locX-140), locY, (locZ+40))
+    createGrave1((locX-140), locY, (locZ+40))
     }
 
     const  createRow3 = (locX, locY, locZ) => { 
-    createGrave3(locX, locY, (locZ+80))
-    createGrave2((locX-20), locY, (locZ+80))
-    // createGrave4((locX+20), locY, (locZ+75))
-    createGrave2((locX-40), (locY), (locZ+80))
-    createGrave3((locX+43), locY, (locZ+80))
-    createGrave4((locX-60), locY, (locZ+80))
-    createGrave4((locX+60), locY, (locZ+80))
-    createGrave3((locX-80), (locY-10), (locZ+80))
-    createGrave2((locX+80), locY, (locZ+80))
-    createGrave2((locX+100), locY, (locZ+80))
-    // createGrave2((locX-100), locY, (locZ+80))
-    createGrave3((locX+120), locY, (locZ+80))
-    createGrave2((locX-120), locY, (locZ+80))
-    createGrave3((locX+140), locY, (locZ+80))
+    createGrave1(locX, locY, (locZ+80))
+    createGrave1((locX-20), locY, (locZ+80))
+    // createGrave1((locX+20), locY, (locZ+75))
+    createGrave1((locX-40), (locY), (locZ+80))
+    createGrave1((locX+43), locY, (locZ+80))
+    createGrave1((locX-60), locY, (locZ+80))
+    createGrave1((locX+60), locY, (locZ+80))
+    createGrave1((locX-80), (locY-10), (locZ+80))
+    createGrave1((locX+80), locY, (locZ+80))
+    createGrave1((locX+100), locY, (locZ+80))
+    // createGrave1((locX-100), locY, (locZ+80))
+    createGrave1((locX+120), locY, (locZ+80))
+    createGrave1((locX-120), locY, (locZ+80))
+    createGrave1((locX+140), locY, (locZ+80))
     }
 
     const  createRow4 = (locX, locY, locZ) => { 
-    createGrave3(locX, locY, (locZ+100))
-    createGrave2((locX-20), locY, (locZ+100))
-    // createGrave4((locX+20), locY, (locZ+75))
-    createGrave2((locX-40), (locY), (locZ+100))
-    createGrave3((locX+43), locY, (locZ+100))
-    createGrave4((locX-60), locY, (locZ+100))
-    createGrave4((locX+60), locY, (locZ+100))
-    createGrave3((locX-80), (locY-10), (locZ+100))
-    createGrave2((locX+80), locY, (locZ+100))
-    createGrave2((locX+100), locY, (locZ+100))
-    createGrave2((locX-100), locY, (locZ+100))
-    createGrave3((locX+120), locY, (locZ+100))
-    createGrave2((locX-120), locY, (locZ+100))
-    createGrave3((locX+140), locY, (locZ+100))
+    createGrave1(locX, locY, (locZ+100))
+    createGrave1((locX-20), locY, (locZ+100))
+    // createGrave1((locX+20), locY, (locZ+75))
+    createGrave1((locX-40), (locY), (locZ+100))
+    createGrave1((locX+43), locY, (locZ+100))
+    createGrave1((locX-60), locY, (locZ+100))
+    createGrave1((locX+60), locY, (locZ+100))
+    createGrave1((locX-80), (locY-10), (locZ+100))
+    createGrave1((locX+80), locY, (locZ+100))
+    createGrave1((locX+100), locY, (locZ+100))
+    createGrave1((locX-100), locY, (locZ+100))
+    createGrave1((locX+120), locY, (locZ+100))
+    createGrave1((locX-120), locY, (locZ+100))
+    createGrave1((locX+140), locY, (locZ+100))
     }
 
     const  createCryptRow = (locX, locY, locZ) => { 
-    createGrave3(locX, locY, (locZ+120))
-    createGrave2((locX-20), locY, (locZ+120))
-    // createGrave4((locX+20), locY, (locZ+75))
-    createGrave2((locX-40), (locY), (locZ+120))
-    createGrave3((locX+43), locY, (locZ+120))
+    createGrave1(locX, locY, (locZ+120))
+    createGrave1((locX-20), locY, (locZ+120))
+    // createGrave1((locX+20), locY, (locZ+75))
+    createGrave1((locX-40), (locY), (locZ+120))
+    createGrave1((locX+43), locY, (locZ+120))
     createCrypt((locX-60), locY, (locZ+120))
-    // createGrave4((locX+60), locY, (locZ+120))
-    // createGrave3((locX-80), (locY-10), (locZ+120))
-    // createGrave2((locX+80), locY, (locZ+120))
-    // createGrave2((locX+100), locY, (locZ+120))
-    createGrave2((locX-100), locY, (locZ+120))
-    createGrave3((locX+120), locY, (locZ+120))
-    createGrave2((locX-120), locY, (locZ+120))
-    createGrave3((locX+140), locY, (locZ+120))
+    // createGrave1((locX+60), locY, (locZ+120))
+    // createGrave1((locX-80), (locY-10), (locZ+120))
+    // createGrave1((locX+80), locY, (locZ+120))
+    // createGrave1((locX+100), locY, (locZ+120))
+    createGrave1((locX-100), locY, (locZ+120))
+    createGrave1((locX+120), locY, (locZ+120))
+    createGrave1((locX-120), locY, (locZ+120))
+    createGrave1((locX+140), locY, (locZ+120))
     }
 
     createRow1(locX, locY, locZ)
