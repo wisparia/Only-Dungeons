b996ca592e45bb23e1f48e1ef0c224935fff702e
ad-fleming <adfleming@gmail.com>
2020-11-15 11:28:14 -0500

    added armor and race contexts, successfully updating state on page load

---
+++ client/src/context/ArmorContext.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ArmorContext = React.createContext({
+    armorState:[],
+  setArmorState: () => {},
+});
+
+export default ArmorContext;
+
+// here is a comment to commit
