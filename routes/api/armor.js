677399d3ab701849f85d0212662075456cc23d2d
ad-fleming <adfleming@gmail.com>
2020-11-15 01:58:51 -0500

    adding line back in

--- routes/api/armor.js
+++ routes/api/armor.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+armorController  = require("../../controllers/armorController")
 router.route("/").get(armorController.getAllArmor);
 
 module.exports = router;
