069bcbae309cc8708e4cfaa5b278d27224883941
samis <samilsully@gmail.com>
2020-11-15 17:23:16 -0800

    added book button to footer and fixed routing to pages

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Footer.css";
+import BookButton from "../BookButton/BookButton";
 
 const footer = () => {
     return (
@@ -8,10 +9,14 @@ const footer = () => {
           <footer className="page-footer">
           <div className="container">
             <div className="row">
-              <div className="col l6 s12">
+            <div className="col s12 l1">
+                <BookButton />
+              </div>
+             
+              <div className="col s12 l6">
                 <p>It's dangerous to go alone.</p>
               </div>
-              <div className="col l4 offset-l2 s12">
+              <div className="col s12 l3 offset-l2">
 
                 <Link className="btn" to="/Team">Meet the Team</Link>
                 <Link className="btn" to="/ThreeD/1">3-D Scene</Link>
